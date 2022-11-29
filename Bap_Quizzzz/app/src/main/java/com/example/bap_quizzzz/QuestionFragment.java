b1e4397685ffd05b9dc6db3a0ce022eed6665d18
cloneson1766 <cloneson1766@gmail.com>
2022-11-29 12:24:09 +0700

    Update 29/11 - Son

--- Bap_Quizzzz/app/src/main/java/com/example/bap_quizzzz/QuestionFragment.java
+++ Bap_Quizzzz/app/src/main/java/com/example/bap_quizzzz/QuestionFragment.java
@@ -8,57 +8,42 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link QuestionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.bap_quizzzz.databinding.FragmentLevelBinding;
+import com.example.bap_quizzzz.databinding.FragmentQuestionBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class QuestionFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    FragmentQuestionBinding binding;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public QuestionFragment() {
-        // Required empty public constructor
+    private void updateQuestionText() {
+        Bundle args = getArguments();
+        Question ques = (Question) args.getSerializable("object_question");
+        binding.questionTextView.setText(ques.getQuestionText());
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment QuestionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static QuestionFragment newInstance(String param1, String param2) {
-        QuestionFragment fragment = new QuestionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question, container, false);
+        binding = FragmentQuestionBinding.inflate(inflater,container,false);
+
+
+        /*Bundle args = getArguments();
+        Question ques = (Question) args.getSerializable("object_question");
+        //binding.questionTextView.setText(math.get(questionIndex).getQuestionText());
+        binding.questionTextView.setText(ques.getQuestionText());*/
+        updateQuestionText();
+
+
+
+        return binding.getRoot();
     }
 }
