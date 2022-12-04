0d066bdf4d809f1a10ebfb4d215e53d99ec39fe4
VanBap <vanbap20021599@gmail.com>
2022-12-04 23:18:11 +0700

    Update 4/12 11:17pm

--- Bap_Quizzzz/app/src/main/java/com/example/bap_quizzzz/QuestionFragment.java
+++ Bap_Quizzzz/app/src/main/java/com/example/bap_quizzzz/QuestionFragment.java
@@ -10,8 +10,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.example.bap_quizzzz.databinding.FragmentQuestionBinding;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -58,6 +61,9 @@ public class QuestionFragment extends Fragment {
         binding.answerTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // HIEU UNG KHI BUTTON DUOC CLICK
+                // YoYo.with(Techniques.RubberBand).duration(1000).repeat(1).playOn(binding.imageView9);
+
                 //check correct answer
                 if(questionItems.get(currentQuestionCount).getAnswer1().equals(questionItems.get(currentQuestionCount).getQuestionAnswer())){
                     HistoryFragment.increaseCorrectCount();
@@ -157,5 +163,8 @@ public class QuestionFragment extends Fragment {
     private void DisplayQuestion(int number){
         binding.questionTextView.setText(questionItems.get(number).getQuestionText());
     }
+
+
+
 
 }
